4e7bd7ee74756dec0f8bf88d2d7b50eb00ede86c
parthjpatel99 <parth8199@gmail.com>
2022-03-16 17:56:49 -0400

    Implemented Intent to Compose Activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -103,7 +104,9 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         if (item.getItemId() == R.id.compose){
-            Toast.makeText(this, "Edit Action", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "Edit Action", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, ComposeActivity.class);
+            startActivity(i);
         }
 
         return super.onOptionsItemSelected(item);
